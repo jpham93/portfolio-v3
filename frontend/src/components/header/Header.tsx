b1d5504bc772e201380f1655c6a8e35cfe98ed41
James Pham <jamespham93@yahoo.com>
2020-12-14 09:52:12 -0800

    Render pages w/o images properly

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -3,8 +3,19 @@ import HeaderPropsModel from '../../models/HeaderProps.model';
 import './Header.scss';
 
 const Header = ({ title, header_image, height }: HeaderPropsModel) => {
+
+  // check to make sure image exist from API
+  let imgUrl = null;
+  if (header_image !== null) {
+    imgUrl = process.env.REACT_APP_API_URL + header_image.url;
+  }
+
+  const headerStyle = imgUrl
+    ? { backgroundImage: `url(${imgUrl})` }
+    : { background: '#0A3D62' };
+
   return(
-    <div className="Header" style={{ backgroundImage: `url(${process.env.REACT_APP_API_URL + header_image.url})` }}>
+    <div className="Header" style={ headerStyle }>
       <h1 className="HeaderTitle">{ title }</h1>
     </div>
   );
