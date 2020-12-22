282879a479f2e7eca999336f5db5b0c247131652
James Pham <jamespham93@yahoo.com>
2020-12-22 09:57:42 -0800

    Custom API endpoint & controller for Project UID

--- backend/api/project/controllers/project.js
+++ backend/api/project/controllers/project.js
@@ -5,4 +5,23 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Custom findOne with the the use of the Project's UID instead of the entry ID
+   * @param ctx             - Strapi Context
+   * @return {Promise<*>}   - HTTP Response (Object or null). Null results in 404.
+   */
+  findOneByProjectId: async ctx => {
+
+    // extract project_id
+    const path        = ctx.url;
+    const regex       = /^\/project\/(.*)$/
+    const project_id       = path.match(regex)[1];
+
+    // query
+    const res         = await strapi.query('project').findOne({ project_id });
+
+    // return res
+    return res;
+  }
+};
