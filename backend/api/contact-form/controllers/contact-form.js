b9017193f7559850b1e48eb35bfb32a6bd5ebdfe
James Pham <jamespham93@yahoo.com>
2021-01-03 13:27:26 -0800

    Rename .env file & add dotenv package

--- backend/api/contact-form/controllers/contact-form.js
+++ backend/api/contact-form/controllers/contact-form.js
@@ -1,4 +1,5 @@
 'use strict';
+const nodemailer = require('nodemailer');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
@@ -9,6 +10,9 @@ module.exports = {
 
   sendContactFormSubmission: async ctx => {
 
+    /**
+     * Verify that contact fields was properly filled out
+     */
     let err = false;
 
     if (!ctx.request.body.hasOwnProperty('name') || !ctx.request.body.name) {
@@ -21,11 +25,25 @@ module.exports = {
       err = true;
     }
 
-    console.log(ctx.request.body);
-
     if (err) {
       ctx.throw(406, 'Contact form submission is missing information.');
     }
+
+    const { name, email, details } = ctx.request.body;
+    console.log(process.env.EMAIL_ACCOUNT);
+
+    // /**
+    //  * Configure Email
+    //  */
+    // let senderAccount = nodemailer.createTransport({
+    //   host: "smtp.ethereal.email",
+    //   port: 587,
+    //   secure: false, // true for 465, false for other ports
+    //   auth: {
+    //     user: testAccount.user, // generated ethereal user
+    //     pass: testAccount.pass, // generated ethereal password
+    //   }
+    // });
 
     return ctx.response;
   }
